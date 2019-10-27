1d5ab25e36478aed721abf3c45c0fc38c465eeaf
Larissa Triess <mail@triess.eu>
2019-10-27 18:25:40 +0100

    Include all functions

--- tf_helpers/training/__init__.py
+++ tf_helpers/training/__init__.py
@@ -3,8 +3,10 @@
 __author__ = """Larissa Triess"""
 __email__ = 'mail@triess.eu'
 
-from .layers import get_padding_sizes
+from .layers import get_padding_sizes, pad, cyclic_padding
 
 __all__= [
     'get_padding_sizes',
+    'pad',
+    'cyclic_padding',
 ]
