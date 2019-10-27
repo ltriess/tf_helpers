ec1a5faeedb19bbbe8ae6decebd1673dd5818650
Larissa Triess <mail@triess.eu>
2019-10-27 17:45:32 +0100

    Update infos

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     version='0.0.1',
     author='Larissa Triess',
     author_email='mail@triess.eu',
-    description='A collection of useful modules when training networks with TensorFlow.',
+    description='A collection of useful functions for TensorFlow for training and visualization.',
     license='MIT',
     packages=[find_packages(exclude=['docs', 'tests'])],
     python_requires='>=3.5',
@@ -19,5 +19,4 @@ setup(
         'tf': ['tensorflow'],
         'tf_gpu': ['tensorflow-gpu'],
     },
-    long_description_content_type='README.md',
 )
